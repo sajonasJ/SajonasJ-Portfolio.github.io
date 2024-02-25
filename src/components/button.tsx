72fdce13cbac584ce60c1334c552875136665b41
Jonas Sajonas <jonas.sajonas@gmail.com>
2024-02-25 19:45:56 +1000

    landing page and info

---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+export default function Button() {
+    return (
+        <div className="box-border
+        h-12
+        w-32
+        bg-slate-300
+        rounded-lg
+        shadow-sm
+        flex
+        items-center
+        justify-center">
+            container
+        </div>
+    )
+}
---
+++ src/components/navigation.tsx
@@ -0,0 +1,10 @@
+import Button from "./button"
+export default function Navigation() {
+    return (
+        <div className="flex justify-center w-full bg-slate-700 h-24 gap-4 items-center">
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+        </div>
+    )
+}
